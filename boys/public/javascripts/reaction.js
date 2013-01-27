b1a2bbd20fcb3843cedd105c9aa3b257a5addb4c
Tony Heupel <tonyheupel@gmail.com>
2013-01-26 23:30:38 -0800

    round 1 done

--- boys/public/javascripts/reaction.js
+++ boys/public/javascripts/reaction.js
@@ -7,7 +7,7 @@ reaction = {
 };
 
 reaction.init = function() {
-  var layer = new Kinetic.Layer({
+  layer = new Kinetic.Layer({
     x: 0,
     y: 100
   });
@@ -28,7 +28,55 @@ reaction.init = function() {
   });
 
   layer.add(this.shape);
+
+  this.winnerText = new Kinetic.Text({
+        x: 0,
+        y: 250,
+        text: 'Congratulations!!!',
+        fontSize: 72,
+        fontFamily: 'Calibri',
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 4,
+        width: 1024,
+        padding: 200,
+        align: 'center',
+        visible: false
+  });
+  layer.add(this.winnerText);
+
+  game.Player.prototype.setColorImage = function(kineticImage) {
+    this.colorImage = kineticImage;
+  }
+  game.Player.prototype.getColorImage = function() {
+    return this.colorImage;
+  }
+  var players = game.players;
+  var imageWidth = 200;
+  var imageHeight = 200;
+  var imageSeparator = 75;
+
+  players.forEach(function(player, index, _array) {
+    var playerImage = new Image();
+    playerImage.addEventListener('load', function() {
+      var colorImage = new Kinetic.Image({
+        x: Math.floor((game.stage.getWidth() - imageWidth) / 2) + (imageWidth/2),
+        y: 50 + (imageHeight/2),
+        offset: [imageWidth/2, imageHeight/2],
+        width: imageWidth,
+        height: imageHeight,
+        image: playerImage,
+        visible: false
+      });
+      layer.add(colorImage);
+      layer.draw();  // Not sure why this is needed other than it happens async
+      player.setColorImage(colorImage);
+    });
+
+    playerImage.setAttribute('src', player.avatar);  // Trigger the load event
+  });
   game.stage.add(layer);
+  this.layer = layer;
 }
 
 reaction.start = function() {
@@ -80,13 +128,42 @@ reaction.start = function() {
 
       if (player.getScore() >= me.toWin) {
         console.log("The winner is..." + player.name + "!!!!");
+        me.showWinner(player);
       } else {
-          me.start();
+        me.showFirst(player, function() { me.start(); });
       }
-    });
+    }, function(key) { console.log('Got key ' + key); });
 
     shape.show();
     console.log("shown!")
     shape.transitionTo({ opacity: 0, duration: 0.5 });
   }, delay * 1000);
 }
+
+reaction.showFirst = function(player, callback) {
+  console.log(player);
+  var image = player.getColorImage();
+  image.show();
+  setTimeout(function() { 
+    image.hide();
+    image.setOpacity(1);
+    callback(); 
+  }, 4000);
+  image.transitionTo({ opacity: 0, duration: 3.5 });
+}
+
+reaction.showWinner = function(player) {
+  this.winnerText.show();
+
+  var winnerImage = player.getColorImage();
+
+  // Rotate once every four seconds
+  var angularSpeed = Math.PI / 2;
+  var anim = new Kinetic.Animation(function(frame) {
+    var angleDiff = frame.timeDiff * angularSpeed / 1000;
+    winnerImage.setOffset(winnerImage.getWidth()/2, winnerImage.getHeight()/2);
+    winnerImage.rotate(angleDiff);
+  }, this.layer);
+
+  winnerImage.show();
+  anim.start(); }
